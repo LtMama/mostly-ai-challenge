e98d23731b5f1a6bf0ed81567972bb51c8c11c1c
Al Pott <alastairp@brandwatch.com>
2023-08-31 15:14:58 +0100

    base setup and lint config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,5 @@ export default defineConfig({
 	watchForFileChanges: false,
 	e2e: {
 		baseUrl: 'https://mostly.ai',
-		setupNodeEvents(on, config) {
-			// implement node event listeners here
-		},
 	},
 });
